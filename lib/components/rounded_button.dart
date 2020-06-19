d04e6e5bd359f25c191a6db339ce29d511302c2d
AnirudhBK8 <58818639+AnirudhBK8@users.noreply.github.com>
2020-06-19 22:25:59 +0530

    Add files via upload

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class RoundedButton extends StatelessWidget {
+  RoundedButton({this.title, this.colour, @required this.onPressed});
+
+  final Color colour;
+  final String title;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedButtonGoogle extends StatelessWidget {
+  RoundedButtonGoogle({this.title, this.colour, @required this.onPressed});
+
+  final Color colour;
+  final String title;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Row(
+            children: <Widget>[
+              Icon(FontAwesomeIcons.google),
+              SizedBox(
+                width: 90.0,
+              ),
+              Center(
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
